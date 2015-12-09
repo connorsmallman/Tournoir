76d6b27987080d32a8dee9bc32c220728bc19d40
Connor Smallman <connor.smallman@bedegaming.com>
2015-12-09 14:07:32 +0000

    Added gulp command for bundlejs

--- src/Tournoir/gulpfile.js
+++ src/Tournoir/gulpfile.js
@@ -1,45 +1,43 @@
-﻿/// <binding Clean='clean' />
-
-var gulp = require("gulp"),
-    rimraf = require("rimraf"),
-    concat = require("gulp-concat"),
-    cssmin = require("gulp-cssmin"),
-    uglify = require("gulp-uglify"),
-    project = require("./project.json");
+﻿var gulp = require('gulp');
+var rimraf = require('rimraf');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var sourcemaps = require('gulp-sourcemaps');
+var gutil = require('gulp-util');
+var project = require('./project.json');
 
 var paths = {
-    webroot: "./" + project.webroot + "/"
+  webroot: './' + project.webroot + '/'
 };
 
-paths.js = paths.webroot + "js/**/*.js";
-paths.minJs = paths.webroot + "js/**/*.min.js";
-paths.css = paths.webroot + "css/**/*.css";
-paths.minCss = paths.webroot + "css/**/*.min.css";
-paths.concatJsDest = paths.webroot + "js/site.min.js";
-paths.concatCssDest = paths.webroot + "css/site.min.css";
+paths.js = {
+  main: paths.webroot + 'js/main.js',
+  dest: paths.webroot + 'dist',
+};
 
-gulp.task("clean:js", function (cb) {
-    rimraf(paths.concatJsDest, cb);
+
+function bundle_js(bundler) {
+  return bundler.bundle()
+    .on('error', gutil.log)
+    .pipe(source('tournoir.js'))
+    .pipe(buffer())
+    .pipe(gulp.dest(paths.js.dest))
+    .pipe(sourcemaps.init({ loadMaps: true }))
+      // capture sourcemaps from transforms
+    .pipe(uglify())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(paths.js.dest));
+}
+
+gulp.task('bundle', function () {
+  var bundler = browserify(paths.js.main, { 
+    debug: true 
+  })
+  .transform(babelify);
+
+  return bundle_js(bundler);
 });
-
-gulp.task("clean:css", function (cb) {
-    rimraf(paths.concatCssDest, cb);
-});
-
-gulp.task("clean", ["clean:js", "clean:css"]);
-
-gulp.task("min:js", function () {
-    gulp.src([paths.js, "!" + paths.minJs], { base: "." })
-        .pipe(concat(paths.concatJsDest))
-        .pipe(uglify())
-        .pipe(gulp.dest("."));
-});
-
-gulp.task("min:css", function () {
-    gulp.src([paths.css, "!" + paths.minCss])
-        .pipe(concat(paths.concatCssDest))
-        .pipe(cssmin())
-        .pipe(gulp.dest("."));
-});
-
-gulp.task("min", ["min:js", "min:css"]);
