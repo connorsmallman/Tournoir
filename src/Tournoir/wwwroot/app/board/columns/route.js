ad51ceee51ba20486ce242d95c03924ec3e787e3
Connor Smallman <connor.smallman@bedegaming.com>
2015-12-14 09:17:53 +0000

    Removed test data

--- src/Tournoir/wwwroot/app/board/columns/route.js
+++ src/Tournoir/wwwroot/app/board/columns/route.js
@@ -4,38 +4,13 @@ import ColumnsView from './collection-view';
 
 export default Route.extend({
   initialize(options = {}) {
-    this.collection = new Collection([ 
-      { 
-        columnId: 1,
-        isLastColumn: false,
-        tiles:  [
-          {
-            tileId: 1,
-            position: 1,
-            nextTileId: 3,
-            player: {
-              playerId: 1,
-              playerName: 'Connor'
-            },
-          },
-          {
-            tileId: 2,
-            position: 2,
-            nextTileId: 3,
-            player: {
-              playerId: 2,
-              playerName: 'Rob'
-            }
-          }
-        ]
-      }
-    ]);
+    this.collection = new Collection();
     this.layout = options.layout;
   },
 
-  // fetch() {
-  //   return this.collection.fetch();
-  // },
+  fetch() {
+    return this.collection.fetch();
+  },
 
   render() {
     this.layout.getRegion('columns').show(new ColumnsView({
